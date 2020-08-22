e3fbf0274b16cc4979c12b9010a31622ccac7318
Dmitriy Sidorov <d_sidorov@wargaming.net>
2020-08-22 20:28:46 +0300

    Connect civic api

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
-require "csv"
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -12,5 +16,17 @@ contents.each do |row|
     name = row[:first_name]
     zipcode=clean_zipcode(row[:zipcode])
 
-    puts "#{name} #{zipcode}"
+
+    begin
+      legislators = civic_info
+        .representative_info_by_address(
+          address: zipcode,
+          levels: 'country',
+          roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+        .officials
+    rescue
+      "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+    end
+
+    puts "#{name} #{zipcode} #{legislators}"
 end
