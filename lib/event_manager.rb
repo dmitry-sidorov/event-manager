a8dd3c092c35782dd7751bec708f1888f2b721e2
Dmitriy Sidorov <d_sidorov@wargaming.net>
2020-08-22 20:41:24 +0300

    Refactoring legaslotors method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,22 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  begin
+    legislators = civic_info
+      .representative_info_by_address(
+        address: zipcode,
+        levels: 'country',
+        roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+      .officials
+
+    legislator_names = legislators.map(&:name).join(', ')
+  rescue
+    legislator_names = "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -13,20 +27,9 @@ puts "EventManager initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-    name = row[:first_name]
-    zipcode=clean_zipcode(row[:zipcode])
+  name = row[:first_name]
+  zipcode=clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-
-    begin
-      legislators = civic_info
-        .representative_info_by_address(
-          address: zipcode,
-          levels: 'country',
-          roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-        .officials
-    rescue
-      "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
-    end
-
-    puts "#{name} #{zipcode} #{legislators}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
