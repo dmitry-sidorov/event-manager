d665dbbe50198cbb74a7a0d7badef942acc68738
Dmitriy Sidorov <d_sidorov@wargaming.net>
2020-08-23 12:48:22 +0300

    ADD erb template

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,15 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
 
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
   begin
     legislators = civic_info
       .representative_info_by_address(
@@ -11,25 +17,33 @@ def legislators_by_zipcode(zipcode)
         levels: 'country',
         roles: ['legislatorUpperBody', 'legislatorLowerBody'])
       .officials
-
-    legislator_names = legislators.map(&:name).join(', ')
   rescue
     legislator_names = "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
 end
 
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, '0')[0..4]
-end
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exists? 'output'
 
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
 
 puts "EventManager initialized!"
 
+template_letter = File.read('form_letter.html')
+erb_template = ERB.new template_letter
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode=clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  save_thank_you_letter(id, form_letter)
 end
