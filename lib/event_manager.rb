4b441979acd3a2038beeb6c23940b15997505de6
Dmitriy Sidorov <d_sidorov@wargaming.net>
2020-08-24 17:04:30 +0300

    Handle phone number

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,23 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def prettify_phone_number(phone_number)
+  "(#{phone_number[0..2]}) #{phone_number[3..5]}-#{phone_number[6..7]}-#{phone_number[8..9]}"
+end
+
+def clean_phone_number(phone_number)
+  formatted_phone_number = phone_number.gsub(/\D+/, '')
+  phone_length = formatted_phone_number.length
+
+  if phone_length == 10
+    prettify_phone_number(formatted_phone_number)
+  elsif phone_length == 11 and formatted_phone_number[0] == '1'
+    prettify_phone_number(formatted_phone_number[1..10])
+  else
+    nil
+  end
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -34,16 +51,18 @@ end
 
 puts "EventManager initialized!"
 
-template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
-contents.each do |row|
+contents.each_with_index do |row, i|
   id = row[0]
   name = row[:first_name]
   zipcode=clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  phone_number = clean_phone_number(row[:homephone])
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
+  puts "#{i} template created!"
 end
